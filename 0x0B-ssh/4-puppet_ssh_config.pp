b6eca05e5ac96a2f63c63d59bbd7d3caaffb7e12
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-10 16:03:10 -0500

    [Fix] added space to the modicated lines

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,10 +2,10 @@
 file_line { 'pass_auth'
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
-    line   => ' PasswordAuthetification no',
+    line   => '    PasswordAuthetification no',
 }
 file_line { 'conf_key'
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
-    line   => ' IdentityFile ~/.ssh/holberton',
+    line   => '    IdentityFile ~/.ssh/holberton',
 }
