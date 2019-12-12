ec70a7506c772368247cd6a4d3f0a261ed5ad949
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-11 23:47:34 -0500

    [Feature] created 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,20 @@
 # Script that installs and configures Nginx
+exec {'update':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y update',
+}
+
 exec {'install':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
   command  => 'sudo apt-get -y install nginx',
+}
+
+exec {'echo_html':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
 exec {'sed_config':
