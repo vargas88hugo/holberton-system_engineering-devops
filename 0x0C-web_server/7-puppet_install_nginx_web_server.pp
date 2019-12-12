355871cb12e423b97904e941f411aa23eea0f485
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-11 23:15:30 -0500

    [Feature] created 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Script that installs and configures Nginx
+exec {'update':
+  command  => apt-get -y update,
+  provider => shell,
+}
+
+exec {'install':
+  command  => apt-get -y install nginx,
+  provider => shell,
+}
+
+exec {'create_html':
+  command  => echo "Holberton School" > /var/www/html/index.nginx-debian.html,
+  provider => shell,
+}
+
+exec {'sed_config':
+  command  => sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default,
+  provider => shell,
+}
+
+exec {'start':
+  command  => service nginx start,
+  provider => shell,
+}
