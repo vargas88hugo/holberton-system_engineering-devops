6e404183ccc95f472547e0ee04753d09785f860d
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-11 23:40:54 -0500

    [Feature] created 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,18 @@
 # Script that installs and configures Nginx
-
 exec {'install':
-  command  => 'sudo apt-get -y install nginx',
   provider => shell,
-  path     => '/usr/bin/env bash',
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y install nginx',
 }
 
 exec {'sed_config':
   command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
   provider => shell,
-  path     => '/usr/bin/env bash',
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
 
 exec {'start':
   command  => 'sudo service nginx start',
   provider => shell,
-  path     => '/usr/bin/env bash',
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
