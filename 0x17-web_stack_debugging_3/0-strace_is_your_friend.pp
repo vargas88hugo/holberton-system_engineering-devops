a3f968f83d8f710ba97c7cee9ca818a4f36f0a61
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-19 08:51:39 -0500

    [Fix] Style had some issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix 500 error in wordpress
 
 exec { 'wp-settings.php':
-   provider => 'shell',
-   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
