ad78078d2bc3561d9ac9a242b6c576aa68aad8cd
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-27 15:44:15 -0500

    [Feature] Created 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This is a puppet script that changes the limits of the files in nginx
 
-exec {
+exec { 'change':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx; sudo service nginx restart'
 }
